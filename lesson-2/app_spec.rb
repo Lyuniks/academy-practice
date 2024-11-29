8a88fc60043218967fa214a7880a6ae3bb96ef6a
Lyuniks <Lyuniks@github.com>
2024-11-29 09:04:09 +0300

    Lesson-2 folder removed

--- lesson-2/app_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'Foobar Method' do
-  describe '#foobar' do
-    it 'правильно запрашивает ввод у пользователя' do
-      allow(self).to receive(:print)
-      allow(self).to receive(:gets).and_return('10')
-      expect(foobar('число')).to eq('10')
-    end
-  end
-end
